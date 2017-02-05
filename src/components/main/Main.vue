aefecd01f5b6ae31cb13237b3c855216ff8df820
hongchh <hongchh_sysu@qq.com>
2017-02-05 11:30:14 +0800

    完善Main.vue

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 div#main
   div#main-header
-    h1
-      router-link(to="/main/home") 病理生理学
+    router-link(to="/main/home")
+      img#header-logo(src="../../assets/logo.png")
+    router-link#header-auth(to="/signin") 登录
   div#main-nav
     div.nav-item#nav-item-0
-      span Course
+      span 课程信息
       div.link-list
         router-link.link-list-item(to="/main/course-introduction") 课程介绍
         router-link.link-list-item(to="/main/course-requirements") 课程要求
@@ -14,7 +15,7 @@ div#main
         router-link.link-list-item(to="/main/teaching-method") 教学方法
         router-link.link-list-item(to="/main/teaching-team") 教学团队
     div.nav-item#nav-item-1
-      span Resources
+      span 课程资源
       div.link-list
         router-link.link-list-item(to="/main/teaching-video") 教学视频
         router-link.link-list-item(to="/main/courseware") 教案课件
@@ -22,17 +23,17 @@ div#main
         router-link.link-list-item(to="/main/teaching-material") 教材信息
         router-link.link-list-item(to="/main/vocabulary") 专业词汇
     div.nav-item#nav-item-2
-      span Achievement
+      span 教学成果
       div.link-list
         router-link.link-list-item(to="/main/achievements") 教学荣誉
         router-link.link-list-item(to="/main/evaluation") 科研成就
         router-link.link-list-item(to="/main/honor") 课程评价
     div.nav-item#nav-item-3
-      router-link.nav-link(to="/main/interaction") Interaction
+      router-link.nav-link(to="/main/interaction") 互动专区
   div#main-body
     router-view
   div#main-footer
-    h1 页脚
+    span 联系电话: 020-87330028 | Copyright 2016 中山大学中山医学院病理生理学教研室 All Right Reserved.
 </template>
 
 <script>
@@ -52,10 +53,21 @@ export default {
   height: 100%
 #main-header
   height: 120px
-  background: blue
+  background: #3b8e06
+  #header-logo
+    height: 100px
+    padding: 10px
+    margin-left: 100px
+  #header-auth
+    font-size: 15px
+    color: white
+    text-decoration: none
+    position: absolute
+    top: 10px
+    right: 10px
 #main-nav
   height: 60px
-  background: green
+  background: #348811
   .nav-item
     position: absolute
     width: 15%
@@ -68,9 +80,10 @@ export default {
       position: absolute
       width: 100%
       top: 60px
-      background: rgba(0, 0, 0, 0.3)
-      z-index: 99
+      background: rgba(0, 0, 0, 0.5)
+      z-index: -1
       line-height: 40px
+      height: 0
       .link-list-item
         display: block
         width: 100%
@@ -79,10 +92,13 @@ export default {
         color: white
         text-decoration: none
       .link-list-item:hover
-        background: rgba(0, 0, 0, 0.3)
+        background: rgba(0, 0, 0, 0.2)
     .nav-link
       color: white
       text-decoration: none
+  .nav-item:hover .link-list
+    height: auto
+    z-index: 99
   #nav-item-0
     left: 15%
   #nav-item-1
@@ -99,5 +115,10 @@ export default {
   padding: 10px
 #main-footer
   height: 100px
-  background: yellow
+  background: #3b8e06
+  text-align: center
+  line-height: 100px
+  color: white
+  span
+    font-size: 15px
 </style>
