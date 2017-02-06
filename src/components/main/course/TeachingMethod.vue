96373be3bef96de004f807c750d55ee8920a310a
hongchh <hongchh_sysu@qq.com>
2017-02-06 22:08:59 +0800

    完成TeachingMethod；开始写TeachingTeam

--- src/components/main/course/TeachingMethod.vue
+++ src/components/main/course/TeachingMethod.vue
@@ -1,10 +1,51 @@
 <template lang="pug">
 div#teaching-method
-  h1 Teaching Method（教学方法）
+  el-row(:gutter="25")
+    el-col(:span="24")
+      el-card.info-card
+        div(slot="header")
+          span.info-card-header 教学方法
+        div.info-card-content {{ teachingMethod.content }}
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'teaching-method'
+  name: 'teaching-method',
+  data () {
+    return {
+      teachingMethod: {
+        content: 'loading ...'
+      }
+    }
+  },
+  created () {
+    axios.get('/data/teaching-method.json').then((res) => {
+      if (res.status === 200) {
+        this.teachingMethod = res.data
+      }
+    })
+  }
 }
 </script>
+
+<style lang="sass">
+#teaching-method .el-row
+  margin-bottom: 20px
+  .info-card
+    width: 100%
+    .info-card-header
+      display: block
+      font-size: 36px
+      padding: 5px
+      margin-left: 15px
+    .info-card-content
+      font-size: 20px
+      margin: 10px
+      padding: 15px
+      border: 1px solid rgba(0, 0, 0, 0.2)
+      border-radius: 8px
+      color: grey
+      white-space: pre-wrap
+</style>
