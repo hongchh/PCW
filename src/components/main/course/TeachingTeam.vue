4d356074212b79213945aa4ec6e0b4c0dcf05d85
hongchh <hongchh_sysu@qq.com>
2017-02-06 22:08:59 +0800

    完成TeachingMethod；开始写TeachingTeam

--- src/components/main/course/TeachingTeam.vue
+++ src/components/main/course/TeachingTeam.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
 div#teaching-team
-  h1 Teaching Team（教学团队）
+  el-row(:gutter="25")
+    el-col(:span="24")
+      el-card.info-card
+        div(slot="header")
+          span.info-card-header 教学团队
+        div.info-card-content
 </template>
 
 <script>
@@ -8,3 +13,23 @@ export default {
   name: 'teaching-team'
 }
 </script>
+
+<style lang="sass">
+#teaching-team .el-row
+  margin-bottom: 20px
+  .info-card
+    width: 100%
+    .info-card-header
+      display: block
+      font-size: 36px
+      padding: 5px
+      margin-left: 15px
+    .info-card-content
+      font-size: 20px
+      margin: 10px
+      padding: 15px
+      border: 1px solid rgba(0, 0, 0, 0.2)
+      border-radius: 8px
+      color: grey
+      white-space: pre-wrap
+</style>
