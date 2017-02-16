f50b7c7022b2c4dabe18ac86b9bd50c24a982327
hongchh <hongchh_sysu@qq.com>
2017-02-16 21:20:47 +0800

    完成CourseRequirementManagement；调整部分样式

--- src/components/management/course/CourseRequirementManagement.vue
+++ src/components/management/course/CourseRequirementManagement.vue
@@ -1,10 +1,51 @@
 <template lang="pug">
 div#course-requirement-management
-  h1 课程要求管理模块
+  el-card(v-for="(val, key) in requirementItems")
+    div.clearfix(slot="header")
+      span.header-text {{ key }}
+      el-button(style="float: right;" type="primary") 保存
+    el-input(type="textarea", autosize, v-model="requirementContent[val]")
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'course-requirement-management'
+  name: 'course-requirement-management',
+  data () {
+    return {
+      requirementItems: {
+        '基本理论': 'theory',
+        '基本技能': 'skill',
+        '教学要求': 'teaching'
+      },
+      requirementContent: {
+        theory: 'loading ...',
+        skill: 'loading ...',
+        teaching: 'loading ...'
+      }
+    }
+  },
+  created () {
+    axios.get('/data/course-requirements.json').then((res) => {
+      if (res.status === 200) {
+        this.requirementContent = res.data
+      }
+    })
+  }
 }
 </script>
+
+<style lang="sass">
+#course-requirement-management
+  .el-card
+    margin-bottom: 20px
+    .clearfix:before, .clearfix:after
+      display: table
+      content: ""
+    .clearfix:after
+      clear: both
+    .header-text
+      line-height: 36px
+      font-size: 24px
+</style>
