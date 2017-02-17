c1d97807c10ba5820fc38772591000732cc02400
hongchh <hongchh_sysu@qq.com>
2017-02-17 15:57:38 +0800

    完成AccountManagement基本结构

---
+++ src/components/management/account/StudentAccountManagement.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+div#student-account-management
+  h1 学生账号管理
+</template>
+
+<script>
+export default {
+  name: 'student-account-management'
+}
+</script>
---
+++ src/components/management/account/UserProfileManagement.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+div#user-profile-management
+  h1 用户个人信息管理
+</template>
+
+<script>
+export default {
+  name: 'user-profile-management'
+}
+</script>
