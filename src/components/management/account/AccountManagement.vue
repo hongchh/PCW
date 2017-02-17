1b705936ce7fc0fd593b9f3463ac28251b2009fb
hongchh <hongchh_sysu@qq.com>
2017-02-17 15:57:38 +0800

    完成AccountManagement基本结构

--- src/components/management/account/AccountManagement.vue
+++ src/components/management/account/AccountManagement.vue
@@ -1,10 +1,30 @@
 <template lang="pug">
 div#account-management
-  h1 Account
+  el-tabs(v-model="activeName")
+    el-tab-pane(label="个人信息" name="userProfile")
+      user-profile-management
+    el-tab-pane(label="学生账号" name="studentAccount")
+      student-account-management
+    el-tab-pane(label="教学团队" name="teachingTeam")
+      teaching-team-management
 </template>
 
 <script>
+import UserProfileManagement from './UserProfileManagement'
+import StudentAccountManagement from './StudentAccountManagement'
+import TeachingTeamManagement from './TeachingTeamManagement'
+
 export default {
-  name: 'account'
+  name: 'account',
+  components: {
+    UserProfileManagement,
+    StudentAccountManagement,
+    TeachingTeamManagement
+  }
 }
 </script>
+
+<style lang="sass">
+#account-management
+  padding: 20px
+</style>
---
+++ src/components/management/account/TeachingTeamManagement.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+div#teaching-team-management
+  h1 教学团队管理
+</template>
+
+<script>
+export default {
+  name: 'teaching-team-management'
+}
+</script>
