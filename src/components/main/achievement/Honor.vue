14147ed6b64e5363edcb83fc4fa9cee42d246f37
骆铭涛 <513301360@qq.com>
2017-02-20 20:47:11 +0800

    完成荣誉评价、教学材料、试卷习题和教材信息

--- src/components/main/achievement/Honor.vue
+++ src/components/main/achievement/Honor.vue
@@ -1,10 +1,40 @@
 <template lang="pug">
 div#honor
   h1 Honor（教学荣誉）
+  el-row(type="flex", justify="center")
+    el-col(:span="20")
+      h2 个人荣誉
+      ul
+        li(v-for="honor in honors")
+          a.link(:href="honor.url", target="_blank") {{ honor.description }}
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'honor'
+  name: 'honor',
+  data () {
+    return {
+      honors: []
+    }
+  },
+  created () {
+    axios.get('/data/honor.json')
+      .then(res => {
+        if (res.status === 200) {
+          this.honors = res.data
+        }
+      })
+  }
 }
 </script>
+
+<style lang="sass">
+@import './style/common.sass'
+
+#honor
+  img
+    height: 100%
+    margin: auto
+</style>
