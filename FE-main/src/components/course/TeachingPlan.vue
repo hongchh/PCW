9d272a444400196c15a771494a0b0ea1bd23a5c3
hongchh <hongchh_sysu@qq.com>
2017-08-05 16:18:20 +0800

    完成主站的教学安排组件

--- FE-main/src/components/course/TeachingPlan.vue
+++ FE-main/src/components/course/TeachingPlan.vue
@@ -1,9 +1,17 @@
 <template lang="pug">
 div.course-module#teaching-plan
-  el-card.info-card
-    div(slot="header")
-      span.info-card-header 教学大纲
-    div.info-card-content {{ teachingPlan.content }}
+  el-row(type="flex", justify="center")
+    el-col(:span="20")
+      h2 教学大纲
+      p(v-if="loading") loading ...
+      ul(v-else)
+        li(v-for="item in syllabus")
+          a.link(:href="item.url", target="_blank") {{ item.title }}
+      h2 教学进度表
+      p(v-if="loading") loading ...
+      ul(v-else)
+        li(v-for="item in schedule")
+          a.link(:href="item.url", target="_blank") {{ item.title }}
 </template>
 
 <script>
@@ -13,15 +21,17 @@ export default {
   name: 'teaching-plan',
   data () {
     return {
-      teachingPlan: {
-        content: 'loading ...'
-      }
+      loading: true,
+      syllabus: null,
+      schedule: null
     }
   },
   created () {
     axios.get('/data/teaching-plan.json').then((res) => {
       if (res.status === 200) {
-        this.teachingPlan = res.data
+        this.loading = false
+        this.syllabus = res.data.syllabus
+        this.schedule = res.data.schedule
       }
     })
   }
@@ -29,5 +39,26 @@ export default {
 </script>
 
 <style lang="sass">
-@import "./style/common.sass"
+#teaching-plan
+  h2
+    padding-bottom: 10px
+    border-bottom: 1px solid #eeeeee
+    font-weight: 400
+    font-size: 24px
+    color: #1f2f3d
+  ul
+    margin-top: 10px
+    margin-bottom: 10px
+  li
+    padding-top: 8px
+    padding-bottom: 8x
+  a.link
+    color: #5e6d82
+    font-size: 16px
+    text-decoration: none
+    transition: all 0.25s
+    position: relative
+    &:hover
+      color: #005FA9
+      text-decoration: underline
 </style>
