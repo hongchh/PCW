9cf0c9d68ce98121cda044ef91da4c3a9f6d7dcf
hongchh <hongchh_sysu@qq.com>
2017-08-13 19:26:24 +0800

    修改主站CourseIntroduction组件样式

--- FE-main/src/components/course/CourseIntroduction.vue
+++ FE-main/src/components/course/CourseIntroduction.vue
@@ -1,9 +1,16 @@
 <template lang="pug">
 div#course-introduction
-  el-card.info-card(v-for="(item, i) in introduction", :key="i")
-    div(slot="header")
-      span.info-card-header {{ item.title }}
-    p.info-card-content {{ item.content }}
+  div.intro-item(v-for="(item, i) in introduction")
+    h2(v-if="i % 2 === 0") {{ '◎ ' + item.title }}
+    h2.right-title(v-else) {{ item.title + ' ◎' }}
+    div(:class="i % 2 !== 0 ? 'line left-line' : 'line right-line'")
+    div.content
+      template(v-if="i % 2 !== 0")
+        p {{ item.content }}
+        img(src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502631462711&di=9b667e21643d8d98b795ad053c05cb60&imgtype=0&src=http%3A%2F%2Fimg.taopic.com%2Fuploads%2Fallimg%2F110919%2F2195-11091915362049.jpg")
+      template(v-else)
+        img(src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502631462711&di=9b667e21643d8d98b795ad053c05cb60&imgtype=0&src=http%3A%2F%2Fimg.taopic.com%2Fuploads%2Fallimg%2F110919%2F2195-11091915362049.jpg")
+        p {{ item.content }}
 </template>
 
 <script>
@@ -27,18 +34,44 @@ export default {
 </script>
 
 <style lang="sass">
-#course-introduction .info-card
-  margin: 15px auto
-  .info-card-header
-    display: block
-    font-size: 18px
-    margin-left: 10px
-  .info-card-content
-    font-size: 14px
-    line-height: 18px
-    margin: 0
-    padding: 10px
-    border: 1px solid rgba(0, 0, 0, 0.2)
-    border-radius: 5px
-    white-space: pre-wrap
+#course-introduction
+  padding: 20px
+  .intro-item
+    margin-bottom: 15px
+    h2
+      border-bottom: none
+      margin-bottom: 8px
+      padding: 0 10px
+    .right-title
+      text-align: right
+    .line
+      height: 6px
+      background: #35904d
+      border-radius: 20px
+      position: relative
+      &:after
+        content: ''
+        width: 0
+        height: 0
+        border-left: 12px solid transparent
+        border-right: 12px solid transparent
+        border-bottom: 25px solid #35904d
+        position: absolute
+        top: -21px
+    .left-line:after
+      left: 0
+    .right-line:after
+      right: 0
+    .content
+      margin-top: 20px
+      display: flex
+      align-items: center
+      img
+        width: 150px
+        height: 150px
+        margin: 10px
+      p
+        flex-grow: 1
+        padding: 10px
+        white-space: pre-wrap
 </style>
