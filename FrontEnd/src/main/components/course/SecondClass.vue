dacec93c06c2a98ad181f01af06f3c30c1f927a8
hongchh <hongchh_sysu@qq.com>
2017-09-14 14:10:53 +0800

    实现第二课堂展示页图片懒加载

--- FrontEnd/src/main/components/course/SecondClass.vue
+++ FrontEnd/src/main/components/course/SecondClass.vue
@@ -17,23 +17,57 @@ div#second-class
 
 <script>
 import axios from 'axios'
+import defaultImg from '../../assets/default.jpg'
 
 export default {
   name: 'second-class',
   data () {
     return {
-      columnImg: [[], [], []]
+      columnImg: [[], [], []],
+      allImgs: null
     }
   },
   created () {
     axios.get('/static/data/second-class.json').then(res => {
       if (res.status === 200) {
+        this.allImgs = []
+        this.columnImg = [[], [], []]
         for (let i = 0, j = 0; i < res.data.length; ++i) {
-          this.columnImg[j].push(res.data[i])
+          this.columnImg[j].push({
+            description: res.data[i].description,
+            url: defaultImg
+          })
+          this.allImgs.push(res.data[i].url)
           j = Number.parseInt((j + 1) % 3)
         }
+        this.loadImgs(0)
       }
     })
+  },
+  methods: {
+    /*
+     * 图片懒加载
+     */
+    loadImgs (index) {
+      let promises = []
+      for (let i = 0; index < this.allImgs.length && i < 3; ++i, ++index) {
+        let col = Number.parseInt(index % 3)
+        let row = Number.parseInt(index / 3)
+        promises.push(new Promise((resolve, reject) => {
+          let img = new Image()
+          img.src = this.allImgs[index]
+          img.onload = () => {
+            this.columnImg[col][row].url = img.src
+            resolve()
+          }
+        }))
+      }
+      Promise.all(promises).then(() => {
+        if (index < this.allImgs.length) {
+          this.loadImgs(index)
+        }
+      })
+    }
   }
 }
 </script>
