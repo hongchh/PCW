414d1348a431db0c4bba296c55c5be9507024ead
hongchh <hongchh_sysu@qq.com>
2017-11-12 20:46:40 +0800

    更新资料；添加静态服务器

--- FrontEnd/config/index.js
+++ FrontEnd/config/index.js
@@ -4,7 +4,7 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    assetsRoot: path.resolve(__dirname, '../../Server/public'),
+    assetsRoot: path.resolve(__dirname, '../../Server/pathophysiology'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: false,
---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const port = 8080
+const compression = require('compression')
+const express = require('express')
+const path = require('path')
+
+let app = express()
+
+app.use(compression())
+app.use('/pathophysiology', express.static(path.join(__dirname, './pathophysiology')))
+app.listen(port)
+
+console.log("http://localhost:" + port)
